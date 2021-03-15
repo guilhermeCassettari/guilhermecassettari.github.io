9280ad1b063624cdbfbd8170964549b044990e8e
Guilherme Gasparotti Cassettari <guizaocassettari@gmail.com>
2021-03-15 13:59:12 -0300

    script

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ menuDev.addEventListener('click', () => {
 
 menuUi.addEventListener('click', () => {
     changePageUi.forEach((item, i) => { setTimeout(function() { addClassActive(item) }, 250 * i) })
-    setTimeout(function() { window.location.replace('ui.html') }, 1300)
+    setTimeout(function() { window.location.replace('/ui.html') }, 1300)
 })
 
 
